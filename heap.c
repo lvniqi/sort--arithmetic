2fe5eebebcf20d2dddb744da5f2421428cacfd5a
lvniqi <lvniqi@gmail.com>
2015-03-25 23:37:20 +0800

    修改堆
    修改堆

--- heap.c
+++ heap.c
@@ -1,4 +1,30 @@
 #include<stdio.h>
+/*
+ *
+ * 新加入节点I 父节点为(i-1)/2
+ * 修复最小堆
+ */
+
+void fixMinHeap(int a[],int child){
+    int parent,temp;
+    temp = a[child];
+    parent = (child-1)/2;
+    while (parent >= 0 && child >0){
+        //如果子节点的值大于父节点
+        //则跳出
+        if(a[parent] <= temp){
+            break;
+        }
+        a[parent] = a[child];
+        child = parent;
+        parent = (child-1)/2;
+    }
+    a[child] = temp;
+}
+void insertMinHeap(int a[],int pos,int value){
+    a[pos] = value;
+    fixMinHeap(a,pos);
+}
 int main(void){
 
 }
